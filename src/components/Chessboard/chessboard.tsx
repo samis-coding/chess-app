e93b293a2fc86c2c6cc9bcfb354d2e2dffffb144
Samiullah <161024844+samis-coding@users.noreply.github.com>
2025-04-12 12:22:46 +0500

    new code

--- src/components/Chessboard/chessboard.tsx
+++ src/components/Chessboard/chessboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import './chessboard.css';
 import Tile from "../Tile/Tile";
@@ -8,19 +8,126 @@ const verticalAxis = ["1","2","3","4","5","6","7","8"]
 const horizontalAxis = ["a","b","c","d","e","f","g","h"]
 
 
+interface Piece{
+    image: string
+    x: number
+    y: number
+}
+
+const  pieces: Piece[] = []
+
+for(let p = 0; p<2; p++){
+    
+    const type = p===0 ? "b" : "w";
+    const y= p===0 ? 7:0;
+
+    pieces.push({image:`assets/images/rook_${type}.png`,x:0,y})
+    pieces.push({image:`assets/images/rook_${type}.png`,x:7,y})
+    pieces.push({image:`assets/images/knight_${type}.png`,x:1,y})
+    pieces.push({image:`assets/images/knight_${type}.png`,x:6,y})
+    pieces.push({image:`assets/images/bishop_${type}.png`,x:2,y})
+    pieces.push({image:`assets/images/bishop_${type}.png`,x:5,y})
+    pieces.push({image:`assets/images/king_${type}.png`,x:4,y})
+    pieces.push({image:`assets/images/queen_${type}.png`,x:3,y})
+}
+
+for(let i = 0;i<8;i++){
+    pieces.push({image:"assets/images/pawn_b.png",x:i,y:6})
+}
+
+
+for(let i = 0;i<8;i++){
+    pieces.push({image:"assets/images/pawn_w.png",x:i,y:1})
+}
+
+
+
+
+
 export default function Chessboard(){
+    
+    const chessboardRef = useRef<HTMLDivElement>(null);
+
+    let activepiece:HTMLElement | null = null;
+
+
+function grabPiece(e: React.MouseEvent<HTMLDivElement,MouseEvent>){
+    const element = e.target as HTMLElement;
+    if(element.classList.contains("chess-piece")){
+        console.log(e);
+
+        const x = e.clientX - 60;
+        const y = e.clientY - 60;
+
+        element.style.position = "absolute";
+
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+
+        activepiece = element;
+
+    }
+
+}
+
+function movepiece(e: React.MouseEvent){
+    const chessboard = chessboardRef.current;
+    const element = e.target as HTMLElement;
+
+    if(activepiece && chessboard){
+        
+        const minX= chessboard.style.left;
+        const minY= chessboard.style.top;
+        const x = e.clientX - 60;
+        const y = e.clientY - 60;
+
+        activepiece.style.position = "absolute";
+
+        activepiece.style.left = `${x}px`;
+        activepiece.style.top = `${y}px`;
+    }
+
+}
+
+function dropPiece(e: React.MouseEvent){
+    if(activepiece){
+        activepiece = null;
+
+    }
+}
+
+
     let board = [];
     for(let j=verticalAxis.length-1;j>=0;j--){
         for(let i = 0;i<horizontalAxis.length;i++)
         {   
             const number = j+i+2;
+            let image=undefined;
 
-            board.push(<Tile number={number}/>)
+            pieces.forEach(p =>{
+                if(p.x === i && p.y === j){
+                    image = p.image;
+                }
+
+            })
+
+            board.push(<Tile key={`${j},${i}`} image={image} number={number}/>)
 
 
           
             
         }
     }
-    return <div id="chessboard">{board}</div>
+    return (
+        <div 
+            onMouseMove={(e) => movepiece(e)} 
+            onMouseDown={(e)=> grabPiece(e)}
+            onMouseUp={(e)=> dropPiece(e)}
+
+            id="chessboard"
+            ref={chessboardRef}
+        >
+            {board}
+        </div>
+        )
 }
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,17 +1,25 @@
 import './Tile.css';
 
 interface Props{
+    image?: string;
     number: number
 }
 
 
-export default function tile({number}: Props){
+export default function tile({number,image}: Props){
 
     if( number %2 === 0){
-        return <div className='tile black-tile'></div>
+        return <div className='tile black-tile'>
+            {image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'></div>}
+            
+        </div>
 
     }else{
-        return <div className='tile white-tile'></div>
+        return <div className='tile white-tile'>
+
+            {image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'></div>}
+        
+        </div>
     }
 
     
